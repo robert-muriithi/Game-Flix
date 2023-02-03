f7f25488de9b5ccca8c1009ad7f44fc2f11378dd
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-03 16:23:01 +0300

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# game_flix_flutter
+
+A flutter game app consuming Rawg API to display popular video games
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/example/game_flix_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_flix_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
