2a9b092cde68a44c5577fb6770d00ea239d02a5c
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-16 06:41:33 +0300

    - feature: create games pages and details pages

---
+++ lib/features/games/presentation/widgets/games_page_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GamesPageTopAppBar extends StatelessWidget {
+  const GamesPageTopAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
