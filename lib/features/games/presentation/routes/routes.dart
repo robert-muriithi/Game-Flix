4afed1c75183af6902c88750d30bdb300d7f78cc
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-16 06:40:32 +0300

    - setup app routes

---
+++ lib/features/games/presentation/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:game_flix_flutter/features/games/presentation/pages/game_details_page.dart';
+import 'package:game_flix_flutter/features/games/presentation/pages/games_page.dart';
+
+class Routes {
+  static const String gamesPage = '/';
+  static const String gameDetailsPage = '/game_details';
+
+  static const initialRoute = gamesPage;
+
+  static final Map<String, WidgetBuilder> routes = {
+    gamesPage: (context) => const GamesPage(),
+    gameDetailsPage: (context) => const GameDetailsPage()
+  };
+
+}
