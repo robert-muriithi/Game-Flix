a196ba846b4f483e9b201fe0defc2def598a95f3
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-18 23:25:01 +0300

    feature: change game data components

--- lib/features/games/data/datasources/local/games_local_datasource.dart
+++ lib/features/games/data/datasources/local/games_local_datasource.dart
@@ -1,13 +1,13 @@
 
 import 'package:game_flix_flutter/core/data/local/database/database.dart';
-import 'package:game_flix_flutter/features/games/data/datasources/local/entity/game_entity.dart';
+import 'package:game_flix_flutter/features/games/data/datasources/local/entity/game/game_entity.dart';
 
 import '../../../../../core/errors/exceptions.dart';
 
 abstract class GamesLocalDataSource {
   Future<void> deleteGames();
-  Future<List<GameEntity>> getGames();
-  Future<void> insertGame(List<GameEntity> games);
+  Future<List<GamesEntity>> getGames();
+  Future<void> insertGame(List<GamesEntity> games);
 }
 
 class GamesLocalDataSourceImpl implements GamesLocalDataSource {
@@ -26,7 +26,7 @@ class GamesLocalDataSourceImpl implements GamesLocalDataSource {
   }
 
   @override
-  Future<List<GameEntity>> getGames()  {
+  Future<List<GamesEntity>> getGames()  {
     try{
       final dao = database.gamesDao;
       final localCache = dao.getAllGames();
@@ -37,7 +37,7 @@ class GamesLocalDataSourceImpl implements GamesLocalDataSource {
   }
 
   @override
-  Future<void> insertGame(List<GameEntity> games) async {
+  Future<void> insertGame(List<GamesEntity> games) async {
     try{
       final dao = database.gamesDao;
       await dao.insertGames(games);
