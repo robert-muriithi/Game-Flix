722fc13dd156efd096158e9239f748c676c5feae
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-18 23:24:00 +0300

    feature: update database, introducing genres table

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,6 +4,7 @@ class Constants {
 
   static const String kDatabaseName = 'game_flix_db.db';
   static const String kGameTableName = 'games_table';
+  static const String kGenresTableName = 'genres_table';
   static const String kFavoritesTableName = 'fav_games_table';
   static const String kBaserUrl = 'https://api.rawg.io/api/';
   static const String apiKey = '9b8ab7bf2f064ff59c6cf6668e6ce8fc';
