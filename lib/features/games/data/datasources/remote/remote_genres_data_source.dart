8dd6ca1d5cf1b18456147724358aa1c4c4dc87fd
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-21 14:15:10 +0300

    - feature: implement remote data source for genres

--- lib/features/games/data/datasources/remote/remote_genres_data_source.dart
+++ lib/features/games/data/datasources/remote/remote_genres_data_source.dart
@@ -2,25 +2,28 @@ import 'package:game_flix_flutter/core/errors/exceptions.dart';
 import 'package:game_flix_flutter/core/utils/constants.dart';
 import 'package:game_flix_flutter/features/games/data/model/games_response.dart';
 import 'package:dio/dio.dart';
-
+import 'package:logger/logger.dart';
 import '../../model/genres_response.dart';
 
 abstract class GenresRemoteDataSource {
   Future<List<GenreResponse>> getGenres();
 }
 
-class GamesRemoteDataSourceImpl extends GenresRemoteDataSource {
+class GenresRemoteDataSourceImpl extends GenresRemoteDataSource {
   final Dio dio;
-  GamesRemoteDataSourceImpl(this.dio);
+  GenresRemoteDataSourceImpl(this.dio);
 
   @override
   Future<List<GenreResponse>> getGenres() async {
+    final log = Logger();
     try{
-      final response = await dio.get(Constants.kBaserUrl);
+      final response = await dio.get('${Constants.kBaserUrl}?key=${Constants.apiKey}');
+      log.d(response.data);
       final data = response.data as List;
       final parsedData = data.map((e) => GenreResponse.fromJson(e)).toList();
       return parsedData;
     }catch (exception){
+      log.e(exception);
       throw ServerException(message: exception.toString());
     }
   }
