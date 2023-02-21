9b788f12c50d092e7d46cdd55716e7fe7e003cb4
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-21 14:13:20 +0300

    - feature: create mappers for genres

--- lib/features/games/data/mappers/mappers.dart
+++ lib/features/games/data/mappers/mappers.dart
@@ -1,4 +1,3 @@
-
 import 'package:game_flix_flutter/features/games/data/datasources/local/entity/genre/genres_entity.dart';
 import 'package:game_flix_flutter/features/games/data/model/genres_response.dart';
 import 'package:game_flix_flutter/features/games/domain/model/genre.dart';
@@ -7,95 +6,83 @@ import '../../domain/model/game.dart';
 import '../datasources/local/entity/game/game_entity.dart';
 import '../model/games_response.dart';
 
-GamesEntity toEntity(GameResponse response){
+GamesEntity toEntity(GameResponse response) {
   return GamesEntity(
       id: response.id,
       name: response.name,
-      backgroundImage: response.backgroundImage
-  );
+      backgroundImage: response.backgroundImage);
 }
 
-GameResponse toResponse(GamesEntity entity){
+GameResponse toResponse(GamesEntity entity) {
   return GameResponse(
+      id: entity.id,
+      name: entity.name,
+      backgroundImage: entity.backgroundImage);
+}
+
+Game toGame(GamesEntity entity) {
+  return Game(
       id: entity.id,
       name: entity.name,
       backgroundImage: entity.backgroundImage
   );
 }
 
-Game toGame(GamesEntity entity){
-  return Game(
-    id: entity.id,
-    name: entity.name,
-    backgroundImage: entity.backgroundImage
-  );
-}
 
-/*Genre toGenre(GenresEntity entity){
-  return Genre(
 
-  );
-}*/
 
-GenresEntity toEntityGenre(GenreResponse response){
+
+GenresEntity toEntityGenre(GenreResponse response) {
   return GenresEntity(
-    count: response.count,
-    next: response.next,
-    previous: response.previous,
-    results: response.results.map((e) => toEntityResults(e)).toList()
+      count: response.count,
+      next: response.next,
+      previous: response.previous,
+      results: response.results.map((e) => toEntityResults(ResultsResponse(
+              id: e.id,
+              name: e.name,
+              gamesCount: e.gamesCount,
+              backgroundImage: e.backgroundImage,
+              game: e.game.map((e) => toResponseGames(
+                  GenresGames(id: e.id, name: e.name))).toList()))).toList()
   );
 }
 
-ResultEntity toEntityResults(ResultsResponse response){
-  return ResultEntity(
-    id: response.id,
-    name: response.name,
-    gamesCount: response.gamesCount,
-    backgroundImage: response.backgroundImage,
-    game: response.game.map((e) => toEntityGames(e)).toList()
-  );
+GenresResults toEntityResults(ResultsResponse response) {
+  return GenresResults(
+      id: response.id,
+      name: response.name,
+      gamesCount: response.gamesCount,
+      backgroundImage: response.backgroundImage,
+      game: response.game
+          .map((e) => toEntityGames(GamesResponse(id: e.id, name: e.name)))
+          .toList());
 }
 
-GamesResponse toResponseGames(GamesEntity entity){
-  return GamesResponse(
-    id: entity.id,
-    name: entity.name
-  );
+GamesResponse toResponseGames(GenresGames entity) {
+  return GamesResponse(id: entity.id, name: entity.name);
 }
 
-
-
-
-GameEntity toEntityGames(GamesResponse response){
-  return GameEntity(
-    id: response.id,
-    name: response.name
-  );
+GenresGames toEntityGames(GamesResponse response) {
+  return GenresGames(id: response.id, name: response.name);
 }
 
-Genre toGenre(GenresEntity entity){
+Genre toGenre(GenresEntity entity) {
   return Genre(
-    count: entity.count,
-    next: entity.next,
-    previous: entity.previous,
-    results: entity.results.map((e) => toResults(e)).toList()
-  );
+      count: entity.count,
+      next: entity.next,
+      previous: entity.previous,
+      results: entity.results.map((e) => toResults(e)).toList());
 }
 
-Results toResults(ResultEntity entity){
+Results toResults(GenresResults entity) {
   return Results(
-    id: entity.id,
-    name: entity.name,
-    gamesCount: entity.gamesCount,
-    backgroundImage: entity.backgroundImage,
-    game: entity.game.map((e) => toGames(e)).toList()
-  );
+      id: entity.id,
+      name: entity.name,
+      gamesCount: entity.gamesCount,
+      backgroundImage: entity.backgroundImage,
+      game: entity.game.map((e) => toGames(e)).toList());
 }
 
-Games toGames(GameEntity entity){
-  return Games(
-    id: entity.id,
-    name: entity.name
-  );
+Games toGames(GenresGames entity) {
+  return Games(id: entity.id, name: entity.name);
 }
-
