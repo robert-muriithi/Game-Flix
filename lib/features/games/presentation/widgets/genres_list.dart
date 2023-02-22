d31ee8e13ea2aef506bc216dd3cd44c4be011240
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-22 11:50:44 +0300

    - feature: modify the ui for genres items list

--- lib/features/games/presentation/widgets/genres_list.dart
+++ lib/features/games/presentation/widgets/genres_list.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import '../../domain/model/genre.dart';
 
 class GenresListWidget extends StatefulWidget {
   const GenresListWidget({Key? key, required this.genres}) : super(key: key);
-  final List<Genre> genres;
+  final List<List<Results>?> genres;
 
   @override
   State<GenresListWidget> createState() => _GenresListWidgetState();
@@ -18,19 +17,52 @@ class _GenresListWidgetState extends State<GenresListWidget> {
       child: Text('No Genres found'));
     }
     return Container(
+      height: 50,
       margin: const EdgeInsets.only(top: 10),
-      child: Row(
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.genres.length,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(5),
+            margin: const EdgeInsets.all(10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              padding: const EdgeInsets.all(5),
+              child: Text(widget.genres[index]!.map((e) => e.name).join(''),
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+          );
+        },
+      ),
+
+      /*Row(
         children: [
           for (var genre in widget.genres)
             Container(
-              margin: const EdgeInsets.only(right: 10),
-              child: Chip(
-                label: Text(genre.results.map((e) => e.name).join(', ')
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.all(10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
+                padding: const EdgeInsets.all(5),
+                child: Text(genre.results!.map((e) => e.name).join(''),
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),),
               ),
             )
         ],
-      )
+      )*/
     );
   }
 }
