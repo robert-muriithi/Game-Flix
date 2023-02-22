bd31aa6b3750a27b963c57f84baeaced8584de22
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-22 11:51:18 +0300

    - feature: modify genre remote data source to return genre as single entity

--- lib/features/games/data/datasources/remote/remote_genres_data_source.dart
+++ lib/features/games/data/datasources/remote/remote_genres_data_source.dart
@@ -1,12 +1,11 @@
 import 'package:game_flix_flutter/core/errors/exceptions.dart';
 import 'package:game_flix_flutter/core/utils/constants.dart';
-import 'package:game_flix_flutter/features/games/data/model/games_response.dart';
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
 import '../../model/genres_response.dart';
 
 abstract class GenresRemoteDataSource {
-  Future<List<GenreResponse>> getGenres();
+  Future<GenreResponse> getGenres();
 }
 
 class GenresRemoteDataSourceImpl extends GenresRemoteDataSource {
@@ -14,14 +13,12 @@ class GenresRemoteDataSourceImpl extends GenresRemoteDataSource {
   GenresRemoteDataSourceImpl(this.dio);
 
   @override
-  Future<List<GenreResponse>> getGenres() async {
+  Future<GenreResponse> getGenres() async {
     final log = Logger();
     try{
-      final response = await dio.get('${Constants.kBaserUrl}?key=${Constants.apiKey}');
-      log.d(response.data);
-      final data = response.data as List;
-      final parsedData = data.map((e) => GenreResponse.fromJson(e)).toList();
-      return parsedData;
+      final response = await dio.get('${Constants.kBaserUrl}genres?key=${Constants.apiKey}');
+      log.i(response.data);
+      return GenreResponse.fromJson(response.data);
     }catch (exception){
       log.e(exception);
       throw ServerException(message: exception.toString());
