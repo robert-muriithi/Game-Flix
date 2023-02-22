dccd23f6284c46f5c679a3cbd17f97bbeae5f836
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-22 11:48:59 +0300

    - feature: modify genres bloc to return list of results

--- lib/features/games/presentation/blocs/games_bloc.dart
+++ lib/features/games/presentation/blocs/games_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:game_flix_flutter/core/usecase/usecase.dart';
 import 'package:game_flix_flutter/features/games/domain/usecase/get_all_genres_usecase.dart';
@@ -17,18 +17,31 @@ part 'games_state.dart';
 class GamesBloc extends Bloc<GamesEvent, GamesState> {
   final GetAllGenresUseCase getGenresUseCase;
 
-  GamesBloc(this.getGenresUseCase) : super(const GamesInitialState()) {
+  GamesBloc(this.getGenresUseCase) : super(GamesInitialState()) {
     on<GetGamesEvent>(getGamesEventObserver);
   }
 
 
-  Future<void> getGamesEventObserver(emit, event) async {
+ /* Future<void> getGamesEventObserver(emit, event) async {
     if(event is GetGamesEvent){
-      emit(const GamesLoadingState());
+      emit(GamesLoadingState());
       final result = await getGenresUseCase.call(NoParams());
       result.fold(
           (failure) => emit(GamesErrorState(message: mapFailureToMessage(failure))),
           (genres) => emit(GamesLoadedState(genres: genres))
+      );
+    }else {
+      emit(const GamesErrorState(message: Constants.UNEXPECTED_FAILURE_MESSAGE));
+    }
+  }*/
+
+  Future<void> getGamesEventObserver(event, emit) async {
+    if(event is GetGamesEvent){
+      emit(GamesLoadingState());
+      final result = await getGenresUseCase(NoParams());
+      result.fold(
+            (failure) => emit(GamesErrorState(message: mapFailureToMessage(failure))),
+            (genres) => emit(GamesLoadedState(results: genres.map((e) => e.results).toList())),
       );
     }else {
       emit(const GamesErrorState(message: Constants.UNEXPECTED_FAILURE_MESSAGE));
