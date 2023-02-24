c9bd8945405d0ed1cb5dc67b4c44445b0f62d97a
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-24 09:17:21 +0300

    - feature: create categories bloc

---
+++ lib/features/categories/presentation/blocs/categories_state.dart
@@ -0,0 +1,38 @@
+part of 'categories_bloc.dart';
+
+
+
+abstract class CategoriesState extends Equatable {
+  const CategoriesState();
+}
+
+class CategoriesInitialState extends CategoriesState {
+  @override
+  List<Object?> get props => [];
+}
+
+class CategoriesLoadingState extends CategoriesState {
+  @override
+  List<Object?> get props => [];
+}
+
+class CategoriesLoadedState extends CategoriesState {
+  const CategoriesLoadedState({required this.results});
+  final List<Results> results;
+
+
+  @override
+  List<Object?> get props => [
+    results
+  ];
+}
+
+class CategoriesErrorState extends CategoriesState {
+  final String message;
+  const CategoriesErrorState({required this.message});
+
+  @override
+  List<Object?> get props => [
+    message
+  ];
+}
