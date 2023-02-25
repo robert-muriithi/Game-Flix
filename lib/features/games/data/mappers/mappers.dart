03a496c8c01b3f8dfa843afa51ccc6c893622e7f
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-25 23:05:15 +0300

    - feature: custom mappers for both games and genres

--- lib/features/games/data/mappers/mappers.dart
+++ lib/features/games/data/mappers/mappers.dart
@@ -1,30 +1,47 @@
-import 'package:game_flix_flutter/features/categories/data/model/genres_response.dart';
-import 'package:game_flix_flutter/features/categories/domain/model/genre.dart';
+import 'package:game_flix_flutter/features/games/data/datasources/local/entity/game/game_entity.dart';
+import 'package:game_flix_flutter/features/games/data/model/games_response.dart';
+import 'package:game_flix_flutter/features/games/domain/model/game.dart';
 
-import '../../../categories/data/datasources/local/entity/genre/genres_entity.dart';
-import '../../domain/model/game.dart';
-import '../datasources/local/entity/game/game_entity.dart';
-import '../model/games_response.dart';
 
-GamesEntity toEntity(GameResponse response) {
-  return GamesEntity(
-      id: response.id,
-      name: response.name,
-      backgroundImage: response.backgroundImage);
+GamesResultsEntity fromResultResponseToEntity(Results results){
+  return GamesResultsEntity(
+      id: results.id!,
+      name: results.name!,
+      released: results.released!,
+      backgroundImage: results.backgroundImage,
+      rating: results.rating!,
+      ratingsCount: results.ratingsCount!,
+      reviewsTextCount: results.reviewsTextCount!,
+      suggestionsCount: results.suggestionsCount!,
+      updated: results.updated!,
+      reviewsCount: results.reviewsCount!,
+      platforms: results.platforms!,
+      genres: results.genres!,
+      esrbRating: results.esrbRating ?? PlatformResult(id: 0, name: 'Not Rated', imageBackground: '', gamesCount: 0),
+      shortScreenshots: results.shortScreenshots!,
+      tags: results.tags!,
+      metaCritic: results.metaCritic!
+  );
 }
 
-GameResponse toResponse(GamesEntity entity) {
-  return GameResponse(
+GameResults fromEntityToDomain(GamesResultsEntity entity){
+  return GameResults(
       id: entity.id,
       name: entity.name,
-      backgroundImage: entity.backgroundImage);
-}
-
-Game toGame(GamesEntity entity) {
-  return Game(
-      id: entity.id,
-      name: entity.name,
-      backgroundImage: entity.backgroundImage
+      released: entity.released,
+      backgroundImage: entity.backgroundImage,
+      rating: entity.rating,
+      ratingsCount: entity.ratingsCount,
+      reviewsTextCount: entity.reviewsTextCount,
+      suggestionsCount: entity.suggestionsCount,
+      updated: entity.updated,
+      reviewsCount: entity.reviewsCount,
+      platforms: entity.platforms,
+      genres: entity.genres,
+      esrbRating: entity.esrbRating,
+      shortScreenshots: entity.shortScreenshots,
+      tags: entity.tags,
+      metaCritic: entity.metaCritic
   );
 }
 
