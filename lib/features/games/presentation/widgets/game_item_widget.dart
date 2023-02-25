2adee95acda83d55f6b8619fcea29e2bc3ed7688
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-25 22:59:06 +0300

    - feature: create game item widget

---
+++ lib/features/games/presentation/widgets/game_item_widget.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:game_flix_flutter/features/games/domain/model/game.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fluttericon/iconic_icons.dart';
+import 'package:fluttericon/fontelico_icons.dart';
+
+import '../../../../config/theme/colors.dart';
+
+class GameItemWidget extends StatelessWidget {
+  const GameItemWidget({Key? key, required this.game}) : super(key: key);
+  final GameResults? game;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.darkGrey,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                  image: CachedNetworkImageProvider( game!.backgroundImage, cacheKey: game!.backgroundImage),
+                  fit: BoxFit.cover,
+                  scale: 2.0,
+                ),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal:8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Text(
+                      game?.name ?? '',
+                      style: const TextStyle(color: AppColors.orange, fontSize: 14),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.orange),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(1),
+                      child: Text(
+                        game!.metaCritic.toString(),
+                        style: const TextStyle(
+                          color: AppColors.orange,
+                          fontSize: 11,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+            color: Colors.white,
+            indent: 10,
+            endIndent: 10,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              children: [
+                const Text(
+                  'Release date',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const Spacer(),
+                Expanded(
+                  child: Text(
+                    game?.released ?? '',
+                    style: const TextStyle(color: Colors.white, fontSize: 12),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              children:  const [
+                Text('Genres', style: TextStyle(color: Colors.white, fontSize: 12),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              children:  const [
+                Text('Genres', style: TextStyle(color: Colors.white, fontSize: 12),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
