537c982d043ec25ca0f4d47fece0d7fce093081c
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-25 23:25:22 +0300

    - feature: implement games usecase and games response

--- lib/features/games/domain/usecase/get_all_game_usecase.dart
+++ lib/features/games/domain/usecase/get_all_game_usecase.dart
@@ -5,13 +5,13 @@ import 'package:game_flix_flutter/features/games/domain/model/game.dart';
 import 'package:game_flix_flutter/features/games/domain/repository/games_repository.dart';
 
 
-class GetAllGamesUseCase extends UseCase<List<Game>, NoParams> {
+class GetAllGamesUseCase extends UseCase<List<GameResults>, Params> {
   final GamesRepository repository;
   GetAllGamesUseCase(this.repository);
 
   @override
-  Future<Either<Failure, List<Game>>> call(NoParams params) async {
-    return repository.getAllGames();
+  Future<Either<Failure, List<GameResults>>> call(Params params) async {
+    return repository.getAllGames(params);
   }
 
 }
