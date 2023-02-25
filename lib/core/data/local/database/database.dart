e665652d8db76edc1ddfaf91b740fc52fb5c4466
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-25 22:55:50 +0300

    - feature: change db entities

--- lib/core/data/local/database/database.dart
+++ lib/core/data/local/database/database.dart
@@ -7,13 +7,23 @@ import 'package:game_flix_flutter/features/games/data/datasources/local/dao/game
 
 import '../../../../features/categories/data/datasources/local/converters/genre_results_type_converter.dart';
 import '../../../../features/categories/data/datasources/local/entity/genre/genres_entity.dart';
+import '../../../../features/games/data/datasources/local/converters/converters.dart';
 import '../../../../features/games/data/datasources/local/entity/game/game_entity.dart';
 
 
 part 'database.g.dart';
 
-@TypeConverters([ResultEntityConverter, GameEntityConverter])
-@Database(version: 1, entities: [GamesEntity, GenresResults])
+@TypeConverters([
+  ResultEntityConverter,
+  GameEntityConverter,
+  PlatFormResultsConverters,
+  PlatformResultTypeConverter,
+  RequirementsEnTypeConverter,
+  GenresResultTypeConverter,
+  TagsResultTypeConverter,
+  ShortScreenshotsResultsConverters,
+])
+@Database(version: 1, entities: [GamesResultsEntity, GenresResults])
 abstract class GameFlixDatabase extends FloorDatabase {
   GamesDao get gamesDao;
   GenresDao get genresDao;
