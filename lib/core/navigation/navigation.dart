11091b45b56585a9e62d832664491c37f97b2f0c
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-26 23:17:13 +0300

    - feature: customize bottom nav

--- lib/core/navigation/navigation.dart
+++ lib/core/navigation/navigation.dart
@@ -18,7 +18,7 @@ class _NavigationContainerState extends State<NavigationContainer> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
-    const GamesPage(),
+    GamesPage(),
     CategoriesPage(),
     const FavouritesPage(),
   ];
@@ -32,184 +32,39 @@ class _NavigationContainerState extends State<NavigationContainer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 7,
-              offset: const Offset(0, 3), // changes position of shadow
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration:   BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30, color: _selectedIndex == 0 ? AppColors.orange : AppColors.darkGrey ,),
+              label: 'Games',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category, size: 30, color: _selectedIndex == 1 ? AppColors.orange : AppColors.darkGrey ,),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, size: 30, color: _selectedIndex == 2 ? AppColors.orange : AppColors.darkGrey ,),
+              label: 'Favourites',
             ),
           ],
-        ),
-        child: BottomAppBar(
-          color: Colors.transparent,
-          child: SizedBox(
-            height: 56,
-            width: MediaQuery.of(context).size.width,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  BottomBarItem(
-                      text: 'Games',
-                      icon: Iconic.home,
-                      selected: _selectedIndex == 0,
-                      onPressed : (){
-                        setState(() {
-                          _selectedIndex == 0;
-                        });
-                      }
-                  ),
-                  BottomBarItem(
-                      text: 'Categories',
-                      icon: Iconic.home,
-                      selected: _selectedIndex == 1,
-                      onPressed : (){
-                        setState(() {
-                          _selectedIndex == 1;
-                        });
-                      }
-                  ),
-                  BottomBarItem(
-                      text: 'Favorites',
-                      icon: Iconic.home,
-                      selected: _selectedIndex == 2,
-                      onPressed : (){
-                        setState(() {
-                          _selectedIndex == 2;
-                        });
-                      }
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      )
-      /*BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, size: 30, color: _selectedIndex == 0 ? AppColors.orange : AppColors.black ,),
-            label: 'Games',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category, size: 30, color: _selectedIndex == 1 ? AppColors.orange : AppColors.black ,),
-            label: 'Categories',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite, size: 30, color: _selectedIndex == 2 ? AppColors.orange : AppColors.black ,),
-            label: 'Favourites',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedLabelStyle: const TextStyle(
-            fontWeight: FontWeight.normal, color: AppColors.orange),
-        unselectedLabelStyle: const TextStyle(
-            fontWeight: FontWeight.normal, color: AppColors.darkGrey),
-        selectedItemColor: AppColors.orange,
-        unselectedItemColor: AppColors.darkGrey,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-      ),*/
-    );
-  }
-}
-
-class CustomBottomNav extends StatefulWidget {
-  const CustomBottomNav({Key? key}) : super(key: key);
-
-  @override
-  State<CustomBottomNav> createState() => _CustomBottomNavState();
-}
-
-class _CustomBottomNavState extends State<CustomBottomNav> {
-  int _selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-       decoration: BoxDecoration(
-         boxShadow: [
-           BoxShadow(
-             color: Colors.grey.withOpacity(0.5),
-             spreadRadius: 1,
-             blurRadius: 7,
-             offset: const Offset(0, 3), // changes position of shadow
-           ),
-         ],
-       ),
-      child: BottomAppBar(
-        color: Colors.transparent,
-        child: SizedBox(
-          height: 56,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BottomBarItem(
-                  text: 'Games',
-                  icon: Iconic.home,
-                  selected: _selectedIndex == 0,
-                  onPressed : (){
-                    setState(() {
-                      _selectedIndex == 0;
-                    });
-                  }
-                ),
-                BottomBarItem(
-                    text: 'Categories',
-                    icon: Iconic.home,
-                    selected: _selectedIndex == 1,
-                    onPressed : (){
-                      setState(() {
-                        _selectedIndex == 1;
-                      });
-                    }
-                ),
-                BottomBarItem(
-                    text: 'Favorites',
-                    icon: Iconic.home,
-                    selected: _selectedIndex == 2,
-                    onPressed : (){
-                      setState(() {
-                        _selectedIndex == 2;
-                      });
-                    }
-                )
-              ],
-            ),
-          ),
+          currentIndex: _selectedIndex,
+          selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.normal, color: AppColors.orange),
+          unselectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.normal, color: AppColors.darkGrey),
+          selectedItemColor: AppColors.orange,
+          unselectedItemColor: AppColors.darkGrey,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
         ),
       ),
-    );
-  }
-}
-
-class BottomBarItem extends StatelessWidget {
-  final String text;
-  final IconData icon;
-  final bool selected;
-  final Function () onPressed;
-  const BottomBarItem({Key? key, required this.text, required this.icon, required this.selected, required this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        IconButton(
-            onPressed: onPressed,
-            icon: Icon(icon, size: 25, color: selected ? AppColors.orange : Colors.black54,)
-        ),
-        Text(text, style: TextStyle(fontSize: 12, height: .1, color:  selected ? AppColors.orange : Colors.black54), )
-      ],
     );
   }
 }
