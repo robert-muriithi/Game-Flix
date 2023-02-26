da7a0f653a6d412625a7675b0cb3367078ef8364
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-26 23:19:32 +0300

    - feature: create a separate remote data source for game details

---
+++ lib/features/games/data/datasources/remote/game_deatils_remote_data_source.dart
@@ -0,0 +1,28 @@
+import 'package:logger/logger.dart';
+import '../../../../../core/errors/exceptions.dart';
+import '../../model/game_details_response.dart';
+import 'package:game_flix_flutter/core/utils/constants.dart';
+import 'package:dio/dio.dart';
+
+
+
+abstract class GameDetailsRemoteDataSource {
+  Future<GameDetailsResponse> getGameDetails({required int id, required String key});
+}
+
+class GameDetailsRemoteDatasourceImpl implements GameDetailsRemoteDataSource {
+  final Dio dio;
+  GameDetailsRemoteDatasourceImpl(this.dio);
+
+  @override
+  Future<GameDetailsResponse> getGameDetails({required int id, required String key}) async {
+    final log = Logger();
+    try{
+     final response = await dio.get('${Constants.kBaserUrl}games/$id?key=$key');
+      return GameDetailsResponse.fromJson(response.data);
+    }catch (exception) {
+      log.e(exception.toString());
+      throw ServerException(message: exception.toString());
+    }
+  }
+}
