99252d8f67ccf93780c3339aba246abe53da59b0
robert-muriithi <njuguiinirobert@gmail.com>
2023-02-27 11:17:05 +0300

    - feature: add more colors for the app

--- lib/config/theme/colors.dart
+++ lib/config/theme/colors.dart
@@ -9,5 +9,7 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color dark = Color(0xFF1A1A1A);
+  static const Color lightGreen = Color(0xFF00E5FF);
+  static const Color red = Color(0xFFE60000);
 
 }
