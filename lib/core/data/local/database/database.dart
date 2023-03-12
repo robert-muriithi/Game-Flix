524edf2ff96adff8a8342a44ec744bb45e69c9f7
robert-muriithi <njuguiinirobert@gmail.com>
2023-03-12 23:54:00 +0300

    - feature: modify db tables

--- lib/core/data/local/database/database.dart
+++ lib/core/data/local/database/database.dart
@@ -1,16 +1,14 @@
 import 'package:floor/floor.dart';
-import 'package:game_flix_flutter/features/categories/data/datasources/local/dao/genres_dao.dart';
 import 'dart:async';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
-import 'package:game_flix_flutter/features/games/data/datasources/local/dao/games_dao.dart';
-
-import '../../../../features/categories/data/datasources/local/converters/genre_results_type_converter.dart';
-import '../../../../features/categories/data/datasources/local/entity/genre/genres_entity.dart';
-import '../../../../features/games/data/datasources/local/converters/converters.dart';
-import '../../../../features/games/data/datasources/local/entity/game/game_entity.dart';
-import '../../../../features/games/data/datasources/local/entity/games_details/games_details.dart';
-
+import '../../../../feature/categories/data/datasources/local/dao/genres_dao.dart';
+import '../../../../feature/games/data/datasources/local/dao/games_dao.dart';
+import '../../../../feature/categories/data/datasources/local/converters/genre_results_type_converter.dart';
+import '../../../../feature/categories/data/datasources/local/entity/genre/genres_entity.dart';
+import '../../../../feature/games/data/datasources/local/converters/converters.dart';
+import '../../../../feature/games/data/datasources/local/entity/game/game_entity.dart';
+import '../../../../feature/games/data/datasources/local/entity/games_details/games_details.dart';
 
 part 'database.g.dart';
 
@@ -25,8 +23,11 @@ part 'database.g.dart';
   ShortScreenshotsResultsConverters,
   DevelopersTypeConverter
 ])
-@Database(version: 1, entities: [GamesResultsEntity, GenresResults, GameDetailsEntity])
+@Database(
+    version: 1,
+    entities: [GamesResultsEntity, GenresResults, GameDetailsEntity])
 abstract class GameFlixDatabase extends FloorDatabase {
   GamesDao get gamesDao;
+
   GenresDao get genresDao;
 }
