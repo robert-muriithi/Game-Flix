1f5fc6674bee7f8f5ea86580cf97b72c1dbdc704
Robaa <njuguiinirobert@gmail.com>
2023-03-17 17:47:11 +0300

    add delete and delete all favorites methods to the repository

--- lib/feature/favourites/domain/repository/favourites_repository.dart
+++ lib/feature/favourites/domain/repository/favourites_repository.dart
@@ -4,6 +4,8 @@ import 'package:game_flix_flutter/core/errors/failure.dart';
 import '../model/favourite.dart';
 
 abstract class FavouriteRepository {
-  Future<Either<Failure, List<Favourite>>> getAllFavorites ();
-  Future<Either<Failure, bool>> addToFavorites(Favourite favourite);
+  Future<Either<Failure, List<Favorites>>> getAllFavorites ();
+  Future<Either<Failure, bool>> addToFavorites(Favorites favourite);
+  Future<Either<Failure, bool>> deleteFavourite(int id);
+  Future<Either<Failure, bool>> deleteAllFavourites();
 }
