2350267549828dca5cef70f961a3005e5b96e76b
Robaa <njuguiinirobert@gmail.com>
2023-03-17 17:48:45 +0300

    duplicate game list widget for categories

---
+++ lib/feature/categories/presentation/widgets/games_list_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:game_flix_flutter/feature/games/domain/model/game.dart';
+
+import 'game_item_widget.dart';
+
+class GamesListWidget extends StatelessWidget {
+  final List<GameResults> games;
+  const GamesListWidget({Key? key, required this.games}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        itemCount: games.length,
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5 / 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 7,
+        ),
+        itemBuilder: (context, index) {
+          final game = games[index];
+          return CategoryGameItemWidget(
+            game: game,
+          );
+        }
+    );
+  }
+}
