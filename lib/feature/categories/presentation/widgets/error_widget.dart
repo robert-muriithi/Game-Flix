c8cce6cf10d978427a2b23c4902147ee64567d18
Robaa <njuguiinirobert@gmail.com>
2023-03-17 17:45:42 +0300

    house keeping

---
+++ lib/feature/categories/presentation/widgets/error_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ErrorMessageWidget extends StatelessWidget {
+  final String message;
+  const ErrorMessageWidget({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.error_outline,
+              color: Colors.red,
+              size: 60,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.red,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
