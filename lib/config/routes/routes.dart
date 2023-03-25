81ccb957869c5b1cfbd1b981efd4fe06ae942086
Robaa <njuguiinirobert@gmail.com>
2023-03-25 23:57:18 +0300

    :feature/search - add more routes

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -11,31 +11,39 @@ import '../../feature/games/domain/model/game.dart';
 import '../../feature/games/presentation/pages/home_page.dart';
 import '../../feature/games/presentation/pages/screenshots_page.dart';
 import '../../feature/onboarding/presentation/pages/onboarding_screens_container.dart';
+import '../../feature/search/domain/model/search_results_model.dart';
+import '../../feature/search/presentation/pages/search_page.dart';
+import '../../feature/search/presentation/pages/search_result_detail_page.dart';
 
 class Routes {
   static const String navigationContainer = '/';
-  static const String gamesPage = '/games';
-  static const String gameDetailsPage =  '/game_details';
-  static const String categoriesPage = '/categories_page';
-  static const String categoryGamesPage = '/categories/games';
-  static const String favoritesPage = '/favourites_page';
-  static const String screenshotsPage = '/game_details/screenshots';
+  static const String homeContainer = '/home_container';
+  static const String gamesPage = '/home_container/games';
+  static const String gameDetailsPage =  '/home_container/games/game_details';
+  static const String categoriesPage = '/home_container/categories';
+  static const String categoryGamesPage = '/home_container/categories/games';
+  static const String favoritesPage = '/home_container/favorites';
+  static const String screenshotsPage = '/home_container/games/game_details/screenshots';
+  static const String searchPage = '/home_container/categories/search';
+  static const String searchResultDetails = '/home_container/categories/search';
 
   static const initialRoute = navigationContainer;
 
   static final Map<String, WidgetBuilder> routes = {
-    //navigationContainer: (context) => const NavigationContainer(),
     navigationContainer: (context) =>  OnBoardingScreensContainer(),
-    //gamesPage: (context) =>  GamesPage(),
-    gamesPage: (context) => const NavigationContainer(),
+    homeContainer: (context) => const NavigationContainer(),
+    gamesPage: (context) =>  GamesPage(),
     gameDetailsPage: (context) => GameDetailsPage(game: ModalRoute.of(context)!.settings.arguments as GameResults),
     categoriesPage: (context) =>  CategoriesPage(),
-    favoritesPage: (context) => const FavouritesPage(),
+    favoritesPage: (context) =>  FavouritesPage(),
     screenshotsPage: (context) =>  ScreenshotsPage(
       screenshot: ModalRoute.of(context)!.settings.arguments as String,
     ),
     categoryGamesPage : (context) =>  CategoryGamesPage(
       genre: ModalRoute.of(context)!.settings.arguments as Results,
+    ),
+    searchResultDetails : (context) =>  SearchResultDetailsPage(
+       id: ModalRoute.of(context)!.settings.arguments as int,
     ),
   };
 
